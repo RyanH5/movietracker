445d6366451d55e11b5bf1f82f7e4b89f661f1e9
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-09 20:06:39 -0600

    Clean movies data

---
+++ src/cleaners/index.js
@@ -0,0 +1,21 @@
+export const movieCleaner = (moviesData) => {
+
+  return moviesData.results.map((movie) => {
+
+    const average = movie.vote_average;
+    const id = movie.id;
+    const popularity = movie.popularity;
+    const posterPath = movie.poster_path;
+    const backDropPath = movie.backdrop_path;
+    const overview = movie.overview;
+
+    return ({
+      average,
+      id,
+      popularity,
+      posterPath,
+      backDropPath,
+      overview
+    })
+  })
+}

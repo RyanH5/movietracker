e8ab556d957bdc5900c4cf6b65c61263b351bf20
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-14 15:59:04 -0600

    Add header proptypes validation

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 
 
-const Header = (props) => {
-  console.log(props.loginStatus);
+const Header = ({loginStatus}) => {
   return (
     <div className="header-section">
       <h1 className="App-title">Movie Tracker</h1>
       {
-        !props.loginStatus ?
+        !loginStatus ?
           <NavLink to="/login" className="nav">Login/SignUp</NavLink> :
           <NavLink to="/" className="nav logout">Log Out</NavLink>
       }
@@ -18,9 +18,14 @@ const Header = (props) => {
   );
 };
 
+
 export const mapStateToProps = (state) => ({
   loginStatus: state.user.loginStatus
 });
+
+Header.propTypes = {
+  loginStatus: PropTypes.bool
+};
 
 
 

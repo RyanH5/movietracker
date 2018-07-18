dd77bddd1408addd20b183b846dd8ec898d00809
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-18 12:01:50 -0600

    Pass MSTP and MDTP tests for login

--- src/Containers/Login/index.test.js
+++ src/Containers/Login/index.test.js
@@ -1,6 +1,7 @@
-import { Login, mapStateToProps, mapDispatchToProps } from './index';
+import { Login, mapStateToProps, mapDispatchToProps } from './';
 import { shallow, mount } from 'enzyme';
 import React from 'react';
+import { toggleUserLogin, userIsFalse, addAllFavs } from '../../Actions';
 
 
 describe('Login', () => {
@@ -46,4 +47,52 @@ describe('Login', () => {
       expect(spy).toHaveBeenCalled();
     });
   });
+  describe('mapStateToProps', () => {
+    it('should create a props object with the correct keys', () => {
+      const mockState = {
+        user: {
+          loginStatus: true,
+          id: 3
+        }
+      };
+      const expected = {
+        loginStatus: true,
+        userId: 3
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+
+    });
+  }); 
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch when using toggleUserLogin from MDTP', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = toggleUserLogin({id: 3, loginStatus: true});
+      const mappedToProps = mapDispatchToProps(mockDispatch);
+      mappedToProps.toggleUserLogin({id: 3, loginStatus: true});
+      
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch when using userIsFalse from MDTP', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = userIsFalse({id: 3, loginStatus: true});
+      const mappedToProps = mapDispatchToProps(mockDispatch);
+      mappedToProps.userIsFalse({id: 3, loginStatus: true});
+      
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch when using addAllFavs from MDTP', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = addAllFavs([{id: 3, name: 'jaws'}]);
+      const mappedToProps = mapDispatchToProps(mockDispatch);
+      mappedToProps.addAllFavs([{id: 3, name: 'jaws'}]);
+      
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
 });
+

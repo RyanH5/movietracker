c855e92dcd4c2f8cf270be61913fc58673c5b7f4
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-18 11:35:14 -0600

    Pass MSTP in cardContainer

--- src/Containers/CardContainer/index.test.js
+++ src/Containers/CardContainer/index.test.js
@@ -1,4 +1,4 @@
-import {CardContainer} from './index';
+import {CardContainer, mapStateToProps} from './index';
 import { shallow } from 'enzyme';
 import React from 'react';
 
@@ -7,5 +7,26 @@ describe('CardContainer', () => {
     let wrapper = shallow(<CardContainer />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return a props object with the correct keys', () => {
+      const mockState = {
+        moviesData: [],
+        user: {
+          favorites: [],
+          loginStatus: true
+        }
+      };
+      const expected = {
+        movies: [],
+        favorites: [],
+        loginStatus: true
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
   });
 });

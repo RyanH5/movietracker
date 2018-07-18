abba1ef5fde7bbe2f7f973870d0de23c77ce5a0a
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-18 11:39:29 -0600

    Pass FavoritesContainer MSTP test

--- src/Containers/FavoritesContainer/index.test.js
+++ src/Containers/FavoritesContainer/index.test.js
@@ -1,6 +1,7 @@
 import {FavoritesContainer} from './index';
 import { shallow } from 'enzyme';
 import React from 'react';
+import { mapStateToProps } from '../FavoritesContainer';
 
 describe('FavoritesContainer', () => {
   it.skip('should match snapshot', () => {
@@ -8,4 +9,21 @@ describe('FavoritesContainer', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('mapStateToProps', () => {
+    it('should create a props object with the correct keys', () => {
+      const mockState = {
+        user: {
+          favorites: []
+        }
+      };
+      const expected = {
+        favorites: []
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
 });
+
